cf05f4f93b006a19969b37a7f00c39f6acadf738
Jon Robson <jdlrobson@gmail.com>
2012-01-11 17:10:56 +0000

    make button overlap bottom right of iframe

--- src/embed.js
+++ src/embed.js
@@ -5,28 +5,32 @@ var container = $('<div id="realtime-widget" />').css({
 	position: "fixed",
 	left: 0,
 	bottom: 0,
-	"border-bottom": "solid 20px #333",
 	"background-color": "#ccc",
 	width: "100%",
 	"border-top": "solid 2px black"
 }).appendTo(document.body)[0];
 
-var height = 150;
+var height = 130;
 
 var widget = $('<iframe id="realtime-widget" src="http://sockets.tiddlyspace.com/index.html" />').appendTo(container);
 $(widget).css({
 	position: "relative",
 	height: height,
+	"z-index": 0,
 	width: "100%"
 });
 
-// offset body so can always see entire document regardless of whether real time window open
-var padding = parseInt($(document.body).css("padding-bottom"), 10);
-var new_padding = padding + height;
-$(document.body).css({ "padding-bottom": new_padding });
-
 $('<button>real time</button>').click(function(ev) {
 	$(widget).toggle();
-}).css({ position: "absolute", right: 0, bottom: "-20px" }).appendTo(container);
+}).css({
+	position: "absolute", right: 0, bottom: "0px",
+	"background": "none",
+	"z-index": 2,
+	"text-indent": -99,
+	bottom: 0,
+	border: "none",
+	"background-repeat": "no-repeat",
+	"background-image": "url(http://following.tiddlyspace.com/SiteIcon)"
+}).appendTo(container);
 
 })(jQuery);
