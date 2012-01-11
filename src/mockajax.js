2db5ffa2661cfd469895b4e80605bc8c3cf8cc18
Jon Robson <jdlrobson@gmail.com>
2012-01-11 11:52:37 +0000

    provide a greater range of mock tiddlers
    this will help us visualise the live stream better and deal with
    things such as different sized site icons..

--- src/mockajax.js
+++ src/mockajax.js
@@ -8,11 +8,28 @@ $.ajax = function(options) {
 			}
 		});
 	} else {
-		var tiddler = {
+		var tiddlers = [{
 			title: "disney music",
 			bag: "disney_public",
 			modifier: "jon"
-		};
-		options.success(tiddler);
+		},
+		{
+			title: "meatloaf",
+			bag: "ben_public",
+			modifier: "bengillies"
+		},
+		{
+			title: "How to lose a laptop in 3 simple steps",
+			bag: "colm_public",
+			modifier: "colmbritton"
+		},
+		{
+			title: "Plugin",
+			bag: "tiddlyspace",
+			modifier: "core",
+			tags: ["systemConfig"]
+		}
+		];
+		options.success(tiddlers[parseInt(Math.random() * tiddlers.length, 10)]);
 	}
 };
