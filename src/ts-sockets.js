7d3941f0e14047682b9506c6fbe39586cba64735
Colm Britton <colmjude@gmail.com>
2012-01-11 10:51:41 +0000

    basic activity item template and one test data example added

--- src/ts-sockets.js
+++ src/ts-sockets.js
@@ -3,3 +3,25 @@ socket.on('news', function (data) {
 	console.log(data);
 	socket.emit('my other event', { my: 'data' });
 });
+
+var testitem = {
+	action: "writing about",
+	modifier: "jon",
+	modifier_url: "http://jon.tiddlyspace.com",
+	modifier_siteicon: "http://jon.tiddlyspace.com/SiteIcon",
+	tiddler_title: "disney music",
+	tiddler_url: "http://jon.tiddlyspace.com/DisneyMusic"
+};
+
+// activity item template
+var itemtemplate = '<div class="activity-item">
+	<a href="{{modifier_url}}">
+		<img src="{{modifier_siteicon}}" />
+	</a>
+	<div>
+		<p>
+			<a href="{{modifier_url}}">{{modifier}}</a> is {{action}}
+			<a class="tiddler-title" href="{{tiddler_url}}">{{tiddler_title}}</a>
+		</p>
+	</div>
+</div>';
