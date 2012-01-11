4a557915fc3d6f7bf3471702aeb10c47a98bc888
Jon Robson <jdlrobson@gmail.com>
2012-01-11 17:59:51 +0000

    ident the entire init function

--- src/ts-sockets.js
+++ src/ts-sockets.js
@@ -50,113 +50,113 @@ function prettyDate(t) {
 }
 
 function init(status) {
-var activity_queue = [];
-var socket = io.connect('http://tiddlyspace.com:8081');
-var el = $("#realtime")[0] || document.body;
-//var container = $("<ul />", {class: "activity-stream"}).appendTo(el);
-var container = $(".activity-stream");
+	var activity_queue = [];
+	var socket = io.connect('http://tiddlyspace.com:8081');
+	var el = $("#realtime")[0] || document.body;
+	//var container = $("<ul />", {class: "activity-stream"}).appendTo(el);
+	var container = $(".activity-stream");
 
-var getVerb = function(tiddler) {
-	var isPlugin = tiddler.tags.indexOf("systemConfig") > -1;
-	var isImage = tiddler.type && tiddler.type.indexOf("image/") === 0;
-	var isCode = tiddler.type && tiddler.type === "text/javascript";
-	var isBookmark = !!tiddler.fields.url;
-	var isPlumbing = PLUMBING.indexOf(tiddler.title) > -1 || tiddler.title.match(/setupFlag$/)
-	var ignoreType = tiddler.type &&
-		(["text/html", "text/css"].indexOf(tiddler.type) > -1 || tiddler.type.indexOf("application/") === 0);
-	if(isPlugin) {
-		action = "shared a plugin called";
-	} else if(isCode) {
-		return "shared a javascript file called ";
-	} else if(isImage) {
-		return "shared an image ";
-	} else if(isBookmark) {
-		return "shared a link - ";
-	} else if(isPlumbing || ignoreType) {
-		return false;
-	} else {
-		return "is writing about";
-	}
-};
+	var getVerb = function(tiddler) {
+		var isPlugin = tiddler.tags.indexOf("systemConfig") > -1;
+		var isImage = tiddler.type && tiddler.type.indexOf("image/") === 0;
+		var isCode = tiddler.type && tiddler.type === "text/javascript";
+		var isBookmark = !!tiddler.fields.url;
+		var isPlumbing = PLUMBING.indexOf(tiddler.title) > -1 || tiddler.title.match(/setupFlag$/)
+		var ignoreType = tiddler.type &&
+			(["text/html", "text/css"].indexOf(tiddler.type) > -1 || tiddler.type.indexOf("application/") === 0);
+		if(isPlugin) {
+			action = "shared a plugin called";
+		} else if(isCode) {
+			return "shared a javascript file called ";
+		} else if(isImage) {
+			return "shared an image ";
+		} else if(isBookmark) {
+			return "shared a link - ";
+		} else if(isPlumbing || ignoreType) {
+			return false;
+		} else {
+			return "is writing about";
+		}
+	};
 
-var toMustacheData = function(tiddler) {
-	var modifier_base = getUrl(status, tiddler.modifier);
-	var origin_space = tiddler.bag.split("_");
-	if(origin_space.length > 1) {
-		var origin_base = getUrl(status, origin_space[0]);
-		var action = getVerb(tiddler);
-		if(!action) {
+	var toMustacheData = function(tiddler) {
+		var modifier_base = getUrl(status, tiddler.modifier);
+		var origin_space = tiddler.bag.split("_");
+		if(origin_space.length > 1) {
+			var origin_base = getUrl(status, origin_space[0]);
+			var action = getVerb(tiddler);
+			if(!action) {
+				return false;
+			}
+			return {
+				action: action,
+				timestamp: tiddler.modified,
+				friendly_date: prettyDate(tiddler.modified),
+				modifier: tiddler.modifier,
+				modifier_url: modifier_base,
+				modifier_siteicon: modifier_base + "SiteIcon",
+				tiddler_title: tiddler.title,
+				tiddler_url: origin_base + encodeURIComponent(tiddler.title)
+			};
+		} else { // ignore things that are not associated with spaces
 			return false;
 		}
-		return {
-			action: action,
-			timestamp: tiddler.modified,
-			friendly_date: prettyDate(tiddler.modified),
-			modifier: tiddler.modifier,
-			modifier_url: modifier_base,
-			modifier_siteicon: modifier_base + "SiteIcon",
-			tiddler_title: tiddler.title,
-			tiddler_url: origin_base + encodeURIComponent(tiddler.title)
-		};
-	} else { // ignore things that are not associated with spaces
-		return false;
-	}
-};
+	};
 
-socket.emit("subscribe", "*");
-socket.on("tiddler", function(data) {
-	var url = data;
-	activity_queue.push(url);
-	updateUI();
-});
+	socket.emit("subscribe", "*");
+	socket.on("tiddler", function(data) {
+		var url = data;
+		activity_queue.push(url);
+		updateUI();
+	});
 
-var addTiddlerToUI = function(tiddler) {
-	var transitionUI = function() {
-		var jf = jQuery(".first"),
-			jm = jQuery(".middle"),
-			jl = jQuery(".last"),
-			jn = jQuery(".next");
+	var addTiddlerToUI = function(tiddler) {
+		var transitionUI = function() {
+			var jf = jQuery(".first"),
+				jm = jQuery(".middle"),
+				jl = jQuery(".last"),
+				jn = jQuery(".next");
 			
-		jl.removeClass("last").addClass("past");
-		jm.removeClass("middle").addClass("last");
-		jf.removeClass("first").addClass("middle");
-		jn.removeClass("next").addClass("first");
-	}
-	var data = toMustacheData(tiddler);
-	if(data) {
-		var html = Mustache.to_html(itemtemplate, data);
-		container.prepend(html);
-		$("#realtime .date").each(function(i, el) {
-			$(el).text(prettyDate($(el).attr("data-timestamp")));
-		});
-		transitionUI();
-	}
-};
-
-var updateUI = function() {
-	if(activity_queue.length > 0) {
-		// get value off queue
-		var url = activity_queue.pop();
-		// perform update
-		$.ajax({
-			url: url,
-			dataType: "json",
-			success: function(tiddler) {
-				addTiddlerToUI(tiddler);
-			}
-		})
-	}
-};
-
-$.ajax({
-	dataType: "json",
-	url: "/search?q=_limit:20",
-	success: function(tiddlers) {
-		for(var i = 0; i < 3 && i < tiddlers.length; i++) {
-			addTiddlerToUI(tiddlers[i]);
+			jl.removeClass("last").addClass("past");
+			jm.removeClass("middle").addClass("last");
+			jf.removeClass("first").addClass("middle");
+			jn.removeClass("next").addClass("first");
 		}
-	}
-})
+		var data = toMustacheData(tiddler);
+		if(data) {
+			var html = Mustache.to_html(itemtemplate, data);
+			container.prepend(html);
+			$("#realtime .date").each(function(i, el) {
+				$(el).text(prettyDate($(el).attr("data-timestamp")));
+			});
+			transitionUI();
+		}
+	};
+
+	var updateUI = function() {
+		if(activity_queue.length > 0) {
+			// get value off queue
+			var url = activity_queue.pop();
+			// perform update
+			$.ajax({
+				url: url,
+				dataType: "json",
+				success: function(tiddler) {
+					addTiddlerToUI(tiddler);
+				}
+			})
+		}
+	};
+
+	$.ajax({
+		dataType: "json",
+		url: "/search?q=_limit:20",
+		success: function(tiddlers) {
+			for(var i = 0; i < 3 && i < tiddlers.length; i++) {
+				addTiddlerToUI(tiddlers[i]);
+			}
+		}
+	})
 
 }
 
